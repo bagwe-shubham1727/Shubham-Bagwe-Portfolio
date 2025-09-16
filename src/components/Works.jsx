c8b33e4f85c0c477770dc019a2a8cb3f325846b8
Shubham Bagwe <bagwe.sh@northeastern.edu>
2025-09-15 21:06:14 -0400

    Github link added for projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,6 +1,6 @@
 import Tilt from "react-tilt";
 import { motion } from "framer-motion";
-
+import { github } from "../assets";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
@@ -11,6 +11,7 @@ const ProjectCard = ({
   name,
   description,
   image,
+  source_code_link
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
@@ -24,7 +25,20 @@ const ProjectCard = ({
             alt={name}
             className="w-full h-full object-cover rounded-2xl"
           />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="github"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
         </div>
+        
         <div className="mt-5">
           <h3 className="text-white font-bold text-[24px]">{name}</h3>
           <p className="mt-2 text-secondary text-[14px]">{description}</p>
